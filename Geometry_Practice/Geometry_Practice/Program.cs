4279fff5df62fe4cf966d9e465072bf099d54cd9
KocsisAGyorgy <gurikocsis31@gmail.com>
2026-01-09 13:33:57 +0100

    Main application logic loop implemented with menu navigation.

--- Geometry_Practice/Geometry_Practice/Program.cs
+++ Geometry_Practice/Geometry_Practice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Geometry_Practice.containers;
+using Geometry_Practice.shapes;
+using System.Text;
 
 namespace Geometry_Practice
 {
@@ -6,7 +8,53 @@ namespace Geometry_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShapeCollection shapes = new ShapeCollection();
+
+            shapes.addShape(new Circle(5.0));
+            shapes.addShape(new Rectangle(4.0, 6.0));
+            shapes.addShape(new Triangle(3.0, 4.0, 5.0));
+            shapes.addShape(new Square(2.0));
+            shapes.addShape(new EquilateralTriangle(3.0));
+            shapes.addShape(new RegularPentagon(2.5));
+
+
+            bool isRunning = true;
+
+            while (isRunning) {
+                showMenu();
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option) {
+                    case 1:
+                        addNewShape(shapes);
+                        break;
+                    case 2:
+                        showAllShapes(shapes);
+                        break;
+                    case 3:
+                        showLargestShapeByPerimeter(shapes);
+                        break;
+                    case 4:
+                        showLargestShapeByArea(shapes);
+                        break;
+                    case 5:
+                        showFormulas();
+                        break;
+                    case 0:
+                        isRunning = false;
+                        Console.WriteLine("Exiting Program.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again!");
+                        break;
+                    
+                }
+                
+            }
+        }
+
+        private static void showMenu() {
+            throw new NotImplementedException();
         }
 
         private static void addNewShape(ShapeCollection shapes) {
